7895d2b8c9913a7dbeb8d29e75d3c25554e73790
bbearcookie <aodem@naver.com>
2023-11-27 02:21:43 +0900

    feat: yaml 파일 읽어서 swaggerUi 에 보여주는 것 성공

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,11 +1,20 @@
-import express from "express"
+import express from 'express';
+import swaggerUi from 'swagger-ui-express';
+import YAML from 'yaml';
+import fs from 'fs';
+import path from 'path';
 
-const app = express()
+const file = fs.readFileSync(path.join(__dirname, './swagger.yaml'), 'utf8');
+const swaggerDocument = YAML.parse(file);
 
-app.get("/", (req, res) => {
-  res.send("Hello World!")
-})
+const app = express();
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
 
 app.listen(5010, () => {
-  console.log("Server running on port 5010")
-})
+  console.log('Server running on port 5010');
+});
